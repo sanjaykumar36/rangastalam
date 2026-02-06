0b6cf874ab8d22b15c73627dc91a1050d27591b7
Sanjay <sanjaythandu2004@gmail.com>
2026-02-06 15:14:17 +0530

    assignment

---
+++ assignment.py
@@ -0,0 +1,57 @@
+# #assignment 1
+# def add(a,b):
+#     return a+b
+# def sub(a,b):
+#     return a-b
+# def mul(a,b):
+#     return a*b
+# def div(a,b):
+#     return a/b
+
+# x= float(input("Enter the first no :  "))
+# y= float(input("Enter the second no:  "))
+# operation=input("Enter the operation:  ")
+
+# if operation=="+":
+#    c=add(x,y)
+#    print(c)
+# elif operation=="-":
+#     c=sub(x,y)
+#     print(c)
+# elif operation=="*":
+#     c=mul(x,y)
+#     print(c)
+# elif operation=="/":
+#      c=div(x,y)
+#      print(c)
+
+
+# #assignment 2
+# def check_even_odd(number):
+#     if number%2==0:
+#         return True
+#     else:
+#         return False
+    
+# c=check_even_odd(999999999999999999999999999999999999999999999999999999999997)
+# print(c)
+
+# #assignment 3
+
+# def display_student_info(name,grade):
+#     print("name: ",name)
+#     print("Grade: ",grade)
+
+# student_name= input("Enter your name :   ")
+# student_grade=input("ente your grade:   ")
+
+# display_student_info(student_name,student_grade)
+
+#assignment 4
+user_input=int(input("enter the number:  "))
+def generate_table(number):
+    number*1
+    number*2
+    return
+generate_table(user_input)
+
